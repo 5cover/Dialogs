9e71bc8548a3a2b3b83f711ac6834ec40c7ddd18
Scover <thediscover22450@gmail.com>
2022-12-17 22:42:27 +0100

    more progress bar madness

--- Dialogs/Parts/ProgressBar.cs
+++ Dialogs/Parts/ProgressBar.cs
@@ -45,7 +45,7 @@ public sealed class ProgressBar : ILayoutProvider<TASKDIALOGCONFIG>, IUpdateRequ
         get => _maximum;
         set
         {
-            _maximum = checked((ushort)value);
+            _maximum = checked((ushort)Math.Max(Value, value));
             RequestRangeUpdate();
         }
     }
@@ -58,8 +58,9 @@ public sealed class ProgressBar : ILayoutProvider<TASKDIALOGCONFIG>, IUpdateRequ
         get => _minimum;
         set
         {
-            _minimum = checked((ushort)value);
+            _minimum = checked((ushort)Math.Min(Value, value));
             RequestRangeUpdate();
+            RequestValueUpdate(); // Needed for some reason.
         }
     }
 
